0efe976098d12d6f125a6d866ee6d1cb6c5ee02f
WesleyAlvess <walvespereira96@gmail.com>
2026-03-18 07:54:15 -0300

    feat: add task filtering with UI controls and improve UX with styled buttons

--- frontend/src/components/tasks/TaskList.jsx
+++ frontend/src/components/tasks/TaskList.jsx
@@ -1,7 +1,34 @@
 import TaskItem from "./TaskItem"
 import Estatisticas from "./Estatisticas";
+import { useState } from "react";
 
 const TaskList = ({ tasks, onToggle, onEdit, onDelete }) => {
+  // useStates
+  // Estado pra filtrar tarefas
+  const [filter, setFilter] = useState("all")
+
+  // Fitrar tasks
+  const filteredTasks = tasks.filter((task) => {
+    if (filter === "all") return true // Todas 
+    if (filter === "Pending") return !task.completed // Pendentes
+    if (filter === "completed") return task.completed // Completas
+
+    if (filter === "expired") { // Expiradas
+      if (!task.deadline) return false
+      return new Date() > new Date(task.deadline)
+    }
+
+    return false
+  })
+
+  // Ordenar a lista de taks por deadline
+  const sortedTasks = [...filteredTasks].sort((a, b) => {
+    if (!a.deadline) return 1 // tarefas sem deadline vão pro final
+    if (!b.deadline) return -1
+
+    return new Date(a.deadline) - new Date(b.deadline) // mais próximo primeiro
+  })
+
   return (
     <div className="space-y-4">
 
@@ -16,7 +43,57 @@ const TaskList = ({ tasks, onToggle, onEdit, onDelete }) => {
           <p className="text-sm text-gray-500">
             Gerencie e acompanhe suas tarefas
           </p>
+
+          {/* Filtro de tarefas */}
+          <div className="flex gap-3 mt-3 text-sm ">
+
+            <button
+              onClick={() => setFilter("all")}
+              className={`px-3 py-1 rounded-full transition-all duration-200
+      ${filter === "all"
+                  ? "bg-gray-800 text-white"
+                  : "bg-gray-100 text-gray-700 hover:bg-gray-200"
+                }`}
+            >
+              Todas
+            </button>
+
+            <button
+              onClick={() => setFilter("pending")}
+              className={`px-3 py-1 rounded-full transition-all duration-200
+      ${filter === "pending"
+                  ? "bg-yellow-400 text-white"
+                  : "bg-yellow-100 text-yellow-700 hover:bg-yellow-200"
+                }`}
+            >
+              Pendentes
+            </button>
+
+            <button
+              onClick={() => setFilter("completed")}
+              className={`px-3 py-1 rounded-full transition-all duration-200 
+      ${filter === "completed"
+                  ? "bg-green-500 text-white"
+                  : "bg-green-100 text-green-700 hover:bg-green-200"
+                }`}
+            >
+              Concluídas
+            </button>
+
+            <button
+              onClick={() => setFilter("expired")}
+              className={`px-3 py-1 rounded-full transition-all duration-200 
+      ${filter === "expired"
+                  ? "bg-red-500 text-white"
+                  : "bg-red-100 text-red-700 hover:bg-red-200"
+                }`}
+            >
+              Vencidas
+            </button>
+
+          </div>
         </div>
+
 
         <Estatisticas tasks={tasks} />
 
@@ -28,7 +105,7 @@ const TaskList = ({ tasks, onToggle, onEdit, onDelete }) => {
           Nenhuma tarefa criada 😔
         </p>
       ) : (
-        tasks.map((task) => (
+        sortedTasks.map((task) => (
           <TaskItem
             key={task._id}
             task={task}
