8a0794d6dc39bf4648b78d0a87312be0694dbcc2
WesleyAlvess <walvespereira96@gmail.com>
2026-03-19 16:12:33 -0300

    fix: connect frontend to production backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:3000/api",
+  baseURL: "https://task-manager-fullstack-x3xn.onrender.com/api",
 });
 
 export const register = (data) => API.post("/auth/register", data);
